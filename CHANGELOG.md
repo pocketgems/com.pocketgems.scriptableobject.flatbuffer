b2700a407be972a5d661606f29ad6599291ffc45
Ashik Raj Manandhar <ashik@pocketgems.com>
2026-01-14 13:42:08 -0800

    Adding Warn support for struct validation (#65)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All package updates & migration steps will be listed in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [4.7.0] - 2025-12-24
+### Added
+- Added Warn support for validation structs
+
 ## [4.6.2] - 2025-12-24
 ### Changed
 - Moved the version key in the `package.json` to a different location.
--- Tests/Runtime/TestCode/TestBaseDataValidatorStruct.cs
+++ Tests/Runtime/TestCode/TestBaseDataValidatorStruct.cs
@@ -7,11 +7,16 @@ namespace PocketGems.Parameters.Validation
         public const string StructPropertyName = "some struct property name";
         public const string ErrorMessage1 = "some message";
         public const string ErrorMessage2 = "some other message";
+        public const string WarningMessage1 = "some warning";
+        public const string WarningMessage2 = "some other warning";
 
         protected override void ValidateStruct(IParameterManager parameterManager, T structObj)
         {
             Error(StructPropertyName, ErrorMessage1);
             Error(ErrorMessage2);
+
+            Warn(StructPropertyName, WarningMessage1);
+            Warn(WarningMessage2);
         }
     }
 }
